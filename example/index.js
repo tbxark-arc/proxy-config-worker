fda87b277e51cdd67b105d0d50d78047a1f3a5ff
TBXark <tbxark@outlook.com>
2020-12-15 17:35:16 +0800

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,11 +1,10 @@
 import Application from '@tbxark/proxy-config-worker/index'
 import HookConfig from '@tbxark/proxy-config-worker/src/hook/config'
 
-async function afterHookConfig(token, port, style, config) {
+
+HookConfig.after = async (token, port, style, config) => {
   return config + '\n\n#Powered by tbxark'
 }
-
-HookConfig.after = afterHookConfig
 
 addEventListener('fetch', event => {
   event.respondWith(Application.handleRequest(event.request))
