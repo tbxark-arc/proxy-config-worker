87419719adca30bbd2f544a53694e08a2f1916e5
TBXark <tbxark@outlook.com>
2020-12-15 17:13:12 +0800

    分离部署代码和核心逻辑

---
+++ example/index.js
@@ -0,0 +1,16 @@
+import Application from '@tbxark/proxy-config-worker'
+import HookConfig from '@tbxark/proxy-config-worker/src/hook/config'
+
+async function afterHookConfig(token, port, style, config) {
+  return config + '\n\n#Powered by tbxark'
+}
+
+HookConfig.after = afterHookConfig
+
+addEventListener('fetch', event => {
+  event.respondWith(Application.handleRequest(event.request))
+})
+
+addEventListener('scheduled', event => {
+  event.waitUntil(Application.handleSchedule(event.scheduledTime))
+})
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Application from './main'
-
-addEventListener('fetch', event => {
-  event.respondWith(Application.handleRequest(event.request))
-})
-
-addEventListener('scheduled', event => {
-  event.waitUntil(Application.handleSchedule(event.scheduledTime))
-})
